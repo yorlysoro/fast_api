3e2420ecb1a1ab58b2eb5a8f7c45ba0189aa70a0
yorlysoro <yorlysoro@gmail.com>
2023-01-16 20:43:13 -0400

    Middlewares

--- main.py
+++ main.py
@@ -25,20 +25,16 @@ from fastapi.security import HTTPBearer
 from config.database import Session, engine, Base
 from models.movie import Movie as MovieModel
 from fastapi.encoders import jsonable_encoder
+from middlewares.error_handler import ErrorHandler
+from middlewares.jwt_bearer import JWTBearer
 
 app = FastAPI()
 app.title = "Aplicacion con FastAPI"
 app.version = "0.0.1"
 
+app.add_middleware(ErrorHandler)
+
 Base.metadata.create_all(bind=engine)
-
-
-class JWTBearer(HTTPBearer):
-    async def __call__(self, request: Request):
-        auth = await super().__call__(request)
-        data = validate_token(auth.credentials)
-        if data['email'] != "admin@gmail.com":
-            raise HTTPException(status_code=403, detail="Not admin")
 
 
 class User(BaseModel):
